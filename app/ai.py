79ca54bac4aa3abac846eee9fe79741b3bfaf442
Sihang Huang <sihangh@nycstudents.net>
2026-06-01 13:52:11 -0400

    2

--- app/ai.py
+++ app/ai.py
@@ -1,29 +1,49 @@
 import requests
+import json
 from dotenv import load_dotenv
 from os import getenv
-import json
 
 load_dotenv()
-AI_KEY = getenv('AI_KEY')
+OPENROUTER_API_KEY = getenv('OPENROUTER_API_KEY')
+
+base_input = """You are an expert in test-making, ready to make practice tests tweaked so that the user can learn most effectively.
+    It needs to be in the json format:
+    {
+        "question number": {
+            "question": question,
+            "answers": {
+                "0": answer
+                ... and so on up to 3.
+            },
+            "correct_answer": index of the correct answer e.g. "0",
+            "explanation": highly thorough explanation.
+        }
+    }
+    You should use questions from reputable sources, like CollegeBoard and such.
+
+"""
 
 def make_test_call(subject: str, question_amount: int):
     response = requests.post(
     url="https://openrouter.ai/api/v1/chat/completions",
     headers={
-        "Authorization": f"Bearer {AI_KEY}",
+        "Authorization": f"Bearer {OPENROUTER_API_KEY}",
         "Content-Type": "application/json",
     },
     data=json.dumps({
-        "model": "nvidia/nemotron-3-super-120b-a12b:free",
+        "model": "openai/gpt-oss-120b:free",
         "messages": [
             {
             "role": "user",
-            "content": f"Give me {question_amount} of multiple-choice questions regarding the subject of {subject}"
+            "content": f"Read this prompt {base_input}. Here is the subject: {subject} and question amount: {question_amount}"
             }
         ],
         "reasoning": {"enabled": True}
     })
     )
+
+    # Extract the assistant message with reasoning_details
     response = response.json()
-    return response['choices'][0]['message']
+    print(response['choices'][0]['message']['content'])
+    #response = response['choices'][0]['message']
 
--- testify.py
+++ testify.py
@@ -1,4 +1,6 @@
 from app import app
+from app.ai import make_test_call
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    #app.run(host="127.0.0.1", port=5000, debug=True)
+    print(make_test_call("Python", 5))
